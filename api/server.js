9701fbe012d715d07f5be562c9b12de8f5b07f2d
Luiz Gustavo <luizg6oliveira@gmail.com>
2026-03-28 01:06:27 -0300

    primeiro commit

---
+++ api/server.js
@@ -0,0 +1,133 @@
+import express from "express";
+import pool from "./database.js";
+
+const app = express();
+app.use(express.json());
+const PORT = 1010;
+
+const connectDB = async () => {
+    try {
+        await pool.getConnection();
+        console.log('Conectado ao Banco de Dados');
+    } catch (error) {
+        console.log('Erro ao conectar ao Banco de Dados', error);
+    }
+}
+
+connectDB();
+
+
+app.post('/usuarios', async (req, res) => {
+    try {
+
+        const { nome, email, senha } = req.body;
+        const[resultado] = await pool.query('INSERT INTO usuarios (nome, email, senha) values (?, ?, ?)', [nome, email, senha]);
+
+        res.json({
+            mensagem: 'Usuario criado com sucesso',
+            id: resultado.insertId
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        const[usuario] = await pool.query('SELECT * FROM usuarios WHERE email = ?', [email]);
+
+        if(usuario.length === 0){
+            return res.json({mensagem: 'Usuario não encontrado'})
+        }
+
+        const user = usuario[0];
+
+        if (senha !== user.senha) {
+            return res.json({ mensagem: 'Senha incorreta' });
+        }
+        
+        res.json({mensagem: "Usuario logado com sucesso"});
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        const[usuario] = await pool.query('SELECT * FROM usuarios');
+
+        if(usuario.length === 0){
+            return res.json({ mensagem: 'Usuario não encontrado' })
+        }
+        
+        res.json(usuario);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/usuarios/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const[usuario] = await pool.query('SELECT * FROM usuarios WHERE id = ?', [id]);
+
+        if (usuario.length === 0) { 
+            return res.json({ mensagem: 'Usuário não encontrado' });
+        }
+
+        res.json(usuario[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const { senha }  = req.body;
+        const { id }  = req.params;
+        const[resultado] = await pool.query('UPDATE usuarios SET senha = ? WHERE id = ? LIMIT 1', [senha, id]);
+
+        if (resultado.affectedRows === 0) {
+            return res.json({
+                mensagem: 'Usuario não encontrado'
+            });
+        };
+
+        res.json({
+            mensagem: 'Senha alterada com sucesso'
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const { nome } = req.params;
+        const { id } = req.params;
+        const[resultado] = await pool.query('DELETE FROM usuarios WHERE id = ?', [id]);
+
+        if (resultado.affectedRows === 0) {
+            return res.json({
+                mensagem: 'Usuario não encontrado'
+            });
+        };
+
+        res.json({
+            mensagem: 'Usuario excluido com sucesso',
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+app.listen(PORT, () => {
+    try {
+        console.log(`O servidor esta conectado na porta ${PORT}`);
+    } catch (error) {
+        console.log('Não foi possivel conectar', error);
+    }
+});
