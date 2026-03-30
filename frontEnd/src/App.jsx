55254e052471555eb40bf028fad1636bd6562cf8
Luiz Gustavo <luizg6oliveira@gmail.com>
2026-03-30 00:03:26 -0300

    fix: Corrigido caminho de import no App.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from  "./pages/Login";
-import Cadastro from "./pages/Cadastro"
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
 
 function App() {
